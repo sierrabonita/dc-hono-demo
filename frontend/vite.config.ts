16833471ae6bda6b449b04732a221563f29334b4
sierrabonita <workbj1212@gmail.com>
2026-06-05 09:08:56 +0900

    feat: Viteの設定にエイリアスを追加

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   server: {
     host: true, // コンテナ外からのアクセスを許可
     port: 5173,
