7c09adeaf11b05513528b0ff6c17c61d7a1781df
sierrabonita <workbj1212@gmail.com>
2026-06-07 13:30:27 +0900

    style: index.cssのインポートを削除

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import '@/index.css';
 import { Client, cacheExchange, fetchExchange, Provider as UrqlProvider } from 'urql';
 import App from '@/App.tsx';
 import { Provider as ChakraProvider } from '@/libs/chakra';
