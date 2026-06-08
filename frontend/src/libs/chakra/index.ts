2cbde8b28b5cbdd23f3b46c7b03b342c6d0ed2ec
sierrabonita <workbj1212@gmail.com>
2026-06-08 11:45:58 +0900

    feat: ConfirmDialogの実装

--- frontend/src/libs/chakra/index.ts
+++ frontend/src/libs/chakra/index.ts
@@ -2,11 +2,14 @@ export {
   Box,
   Button,
   Center,
+  CloseButton,
   Container,
+  Dialog,
   Flex,
   Heading,
   Input,
   Link,
+  Portal,
   Stack,
 } from '@chakra-ui/react';
 export { Field } from '@/libs/chakra/field';
