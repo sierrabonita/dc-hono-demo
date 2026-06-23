78cc164bbd4bf6fa337702aa783d92d8b408be5b
sierrabonita <workbj1212@gmail.com>
2026-06-24 08:43:50 +0900

    feat: usersTableでinteger()などの第1引数を修正(スネークケースにする必要があるもののみ記載)

--- backend/src/db/schema/users.ts
+++ backend/src/db/schema/users.ts
@@ -3,11 +3,11 @@ import { sql } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const usersTable = sqliteTable('users', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
-  name: text('name').notNull(),
-  email: text('email').unique().notNull(),
-  password: text('password').notNull().default('temporary_password'),
-  role: integer('role').$type<UserRole>().notNull().default(0),
+  id: integer().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  email: text().unique().notNull(),
+  password: text().notNull().default('temporary_password'),
+  role: integer().$type<UserRole>().notNull().default(0),
   createdAt: text('created_at').default(sql`CURRENT_TIMESTAMP`).notNull(),
   updatedAt: text('updated_at').default(sql`CURRENT_TIMESTAMP`).notNull(),
 });
