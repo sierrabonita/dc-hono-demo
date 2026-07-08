a7ad954f26449cc9683335b620608d8b76cf0cfd
sierrabonita <workbj1212@gmail.com>
2026-07-09 08:33:55 +0900

    feat: Headerにアイコン追加

--- frontend/src/pages/top/_components/Header.tsx
+++ frontend/src/pages/top/_components/Header.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
+import { BiSolidCameraMovie } from 'react-icons/bi';
 import SimpleDialog from '@/components/dialogs/SimpleDialog';
 import LoginForm from '@/components/forms/LoginForm';
-import { Box, Heading, HStack, Link } from '@/libs/chakra';
+import { Heading, HStack, Link } from '@/libs/chakra';
 
 export const Header = () => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
@@ -11,11 +12,12 @@ export const Header = () => {
       <HStack px="4" py="2" justifyContent="flex-end" alignItems="center">
         <Link onClick={() => setIsDialogOpen(true)}>ログイン</Link>
       </HStack>
-      <Box px="4" py="2" backgroundColor="black">
+      <HStack px="4" py="2" backgroundColor="black">
+        <BiSolidCameraMovie color="yellow" size="48" />
         <Heading fontSize="4xl" fontWeight="bold" color="white">
           Cinema Review
         </Heading>
-      </Box>
+      </HStack>
       <SimpleDialog isOpen={isDialogOpen} onOpenChange={setIsDialogOpen}>
         <LoginForm />
       </SimpleDialog>
