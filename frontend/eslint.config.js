1512535a14251fc0fa85bb7d1eaf7325f881f8b9
sierrabonita <workbj1212@gmail.com>
2026-08-02 12:47:28 +0900

    feat: eslint-plugin-import-x 追加し関連箇所を修正

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintPluginImportX from 'eslint-plugin-import-x';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
@@ -17,6 +18,25 @@ export default defineConfig([
     ],
     languageOptions: {
       globals: globals.browser,
+    },
+  },
+  eslintPluginImportX.flatConfigs.recommended,
+  eslintPluginImportX.flatConfigs.typescript,
+  {
+    rules: {
+      'import-x/default': 'error',
+      'import-x/named': 'error',
+      'import-x/no-unresolved': 'error',
+      'import-x/no-named-as-default': 'off',
+      'import-x/no-named-as-default-member': 'off',
+    },
+    settings: {
+      'import-x/resolver': {
+        typescript: {
+          project: ['./tsconfig.app.json', './tsconfig.node.json'],
+          alwaysTryTypes: true,
+        },
+      },
     },
   },
 ]);
